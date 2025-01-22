21ea599a027583ba57a0324f823288b405330ae4
palmate09 <palmateshubham9@gmail.com>
2025-01-22 13:37:46 +0530

    first commit

---
+++ practice1.js
@@ -0,0 +1,26 @@
+let currentSpeed = 0; 
+
+function increaseSpeed(){
+    currentSpeed ++;
+    
+    document.getElementById("Verticalspeed").innerHTML = `Vh = 
+    ${currentSpeed} [km/s]`;
+}
+
+
+function increaseSpeed1(){
+    currentSpeed ++;
+    
+    document.getElementById("Horizontalspeed").innerHTML = `Vh = 
+    ${currentSpeed} [km/s]`;
+}
+
+
+
+
+
+
+
+
+
+

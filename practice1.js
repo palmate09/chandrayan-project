c48fb51658204680330c07c2c5c8ab9826dc4678
root <root@Shubham>
2025-01-23 14:05:58 +0530

    second commit

--- practice1.js
+++ practice1.js
@@ -1,20 +1,25 @@
-let currentSpeed = 0; 
+// let currentSpeed = 0; 
 
-function increaseSpeed(){
-    currentSpeed ++;
+// function increaseSpeed(){
+//     currentSpeed ++;
     
-    document.getElementById("Verticalspeed").innerHTML = `Vh = 
-    ${currentSpeed} [km/s]`;
-}
+//     document.getElementById("Verticalspeed").innerHTML = `Vh = 
+//     ${currentSpeed} [km/s]`;
+// }
 
 
-function increaseSpeed1(){
-    currentSpeed ++;
+// function increaseSpeed1(){
+//     currentSpeed ++;
     
-    document.getElementById("Horizontalspeed").innerHTML = `Vh = 
-    ${currentSpeed} [km/s]`;
-}
+//     document.getElementById("Horizontalspeed").innerHTML = `Vh = 
+//     ${currentSpeed} [km/s]`;
+// }
 
+const image = document.querySelector("image"); 
+const Horizontalspeed = document.querySelector("horizontal speed"); 
+const Verticalspeed = document.querySelector("Vertical speed"); 
+const curvedln = document.querySelector("curved-line1");
+ 
 
 
 
